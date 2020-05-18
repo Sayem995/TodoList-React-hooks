f6e9136c5d3fc5290dede1dc12d4e7ca90c9752a
Sayem <sayem.bokhari.1@gmail.com git config --global user.email sayem.bokhari.1@gmail.com>
2020-05-18 13:53:05 +0500

    initial set up, created components folder and added files to it

---
+++ react-todo-list/src/components/TodoForms.js
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+
+function TodoForms() {
+    const [todo, setTodo] = ({
+        id: "",
+        task: "",
+        completed: false
+    });
+
+    return (
+        <form>
+            <input />
+            <button />
+        </form>
+    );
+}
+
+export default TodoForms;
