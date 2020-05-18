955c3c8fb670fcaa599f3aa9da641f03a01d944e
Sayem <sayem.bokhari.1@gmail.com git config --global user.email sayem.bokhari.1@gmail.com>
2020-05-18 14:25:26 +0500

    adding functions to TodoForms

--- react-todo-list/src/App.js
+++ react-todo-list/src/App.js
@@ -1,8 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import TodoForm from "./components/TodoForm";
 
 function App() {
+
+  const [todos, setTodos] = useState([]);
+
+  function addTodo(todo) {
+    setTodos([todo, ...todo]);
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
