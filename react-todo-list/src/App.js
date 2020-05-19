08f3c7d466dad83fd415c4bb6b943bd8495e5ee4
Sayem995 <sayem.bokhari.1@gmail.com>
2020-05-19 14:14:18 +0500

    deleted files, started over

--- react-todo-list/src/App.js
+++ react-todo-list/src/App.js
@@ -1,25 +1,47 @@
+// import React, { useState } from "react";
+// import './App.css';
+// import TodoForm from "./components/TodoForm";
+// import TodoList from "./components/TodoList";
+
+// function App() {
+
+//   const [todos, setTodos] = useState([]);
+
+//   function addTodo(todo) {
+//     setTodos([todo, ...todos]);
+//   }
+
+
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//        <p>React Todo</p>
+//        <TodoForm addTodo={addTodo} />
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
 import React, { useState } from "react";
 import './App.css';
-import TodoForm from "./components/TodoForm";
-import TodoList from "./components/TodoList";
 
 function App() {
-
-  const [todos, setTodos] = useState([]);
-
-  function addTodo(todo) {
-    setTodos([todo, ...todos]);
-  }
-
+  const [newTodo, setNewTodo] = useState('')
 
   return (
-    <div className="App">
-      <header className="App-header">
-       <p>React Todo</p>
-       <TodoForm addTodo={addTodo} />
-      </header>
+    <div className = "demo-component">
+      <h1>Todo List 2</h1>
+      <form>
+        <input placeholder="Add Todo task..." />
+        <ul>
+          <li>Add styling to your app</li>
+          <li>Finish todo app today</li>
+        </ul>
+      </form>
     </div>
-  );
+  )
 }
 
 export default App;
