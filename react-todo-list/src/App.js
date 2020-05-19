cc5e305666158f09488fb6ff48653e5c9f32d7b9
Sayem995 <sayem.bokhari.1@gmail.com>
2020-05-19 15:54:14 +0500

    trying to implement delete feature

--- react-todo-list/src/App.js
+++ react-todo-list/src/App.js
@@ -14,20 +14,27 @@ function App() {
     e.preventDefault()
     if (newTodo === "") return
     setTodos([...todos, { text: newTodo}])
-    
+  }
+
+  function removeNewTodo(e) {
+    e.preventDefault()
+    newTodo.splice(e.target.value);
+    setTodos([...todos, { text: newTodo}])
   }
 
   return (
     <div className = "Todo-lists">
       <h1> Todo List 2</h1>
       <div className= "header">
-        <button onClick={handleNewTodo} type="submit" id="btn">Add</button>
         <input placeholder="Add Todo task..." onChange={handleNewTodoChange} />
+        <button onClick={handleNewTodo} type="submit" id="btn">Add</button>
         <ul>
           {todos.map((todo) => (
             <li>{todo.text}</li>
           ))}
+          <button onClick={removeNewTodo} type="submit" id="btn">Delete</button>
         </ul>
+        
         </div> 
     </div>
   )
