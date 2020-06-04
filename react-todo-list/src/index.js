4ef3c0b72b04f414c7d99d609a6cc8eda66724e2
Sayem995 <sayem.bokhari.1@gmail.com>
2020-06-04 09:12:56 +0500

    imported the components

--- react-todo-list/src/index.js
+++ react-todo-list/src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import TaskDetail from './components/TaskDetail';
+import TaskList from './components/TaskList';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
