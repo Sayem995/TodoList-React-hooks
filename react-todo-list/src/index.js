6bd42c526eaafbdc450ab40016046877edfbd27d
Sayem995 <sayem.bokhari.1@gmail.com>
2020-06-04 09:25:22 +0500

    implementing routing

--- react-todo-list/src/index.js
+++ react-todo-list/src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
 import './index.css';
 import App from './App';
 import TaskDetail from './components/TaskDetail';
 import TaskList from './components/TaskList';
+
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
