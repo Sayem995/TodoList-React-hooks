a0edb1f9cce8fb1ad22847433a59e94209f4050c
Sayem995 <sayem.bokhari.1@gmail.com>
2020-06-05 14:21:44 +0500

    removed import TaskList

--- react-todo-list/src/index.js
+++ react-todo-list/src/index.js
@@ -4,7 +4,7 @@ import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
 import './index.css';
 import App from './App';
 import TaskDetail from './components/TaskDetail';
-import TaskList from './components/TaskList';
+
 
 import * as serviceWorker from './serviceWorker';
 
@@ -26,7 +26,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
